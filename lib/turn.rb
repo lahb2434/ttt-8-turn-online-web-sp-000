9210da426592fef8dc6225004de628718c8dfe16
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-11 00:39:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
   user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(board, index) 
-    move(board, cell, )
+    move(board, cell, player)
     
   end
 end
